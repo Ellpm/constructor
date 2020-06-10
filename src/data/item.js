54f59d31c9ffb43cd18fa6dece83467768e5939e
Ellpm <ellpm@ya.ru>
2020-06-10 14:18:50 +0300

    add structure

---
+++ src/data/item.js
@@ -0,0 +1,9 @@
+export const item = {
+         id: 1,
+         class: "Standart",
+         power: "59 Вт.",
+         intensity: "3459 Люмен = 7,5 ламп накаливания по 40 Вт.",
+         garanty: 3,
+         installation: "Да",
+         price: 2594,
+       }; 
